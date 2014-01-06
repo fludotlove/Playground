bdcf136f3a3adb8f7783758e05613eaf7b871575
Nathan Marshall <nathanm@studioskylab.com>
2014-01-06 14:30:14 +0000

    fixed issue with preview tags

--- List/preview.php
+++ List/preview.php
@@ -8,8 +8,8 @@ require 'parsedown.php';
 
 $pd = Parsedown::instance();
 $item = isset($_POST['item']) ? $_POST['item'] : '';
+$item = preg_replace('/\#(\w+)/', '<a class="tag" href="?tag=$1">$1</a>', $item);
 
 $parsed = $pd->parse($item);
-$parsed = preg_replace('/\#(\w+)/', '<a class="tag" href="?tag=$1">$1</a>', $parsed);
 
 echo $parsed;
